4a826b0f612425ee5282f92ffd20a13f2ba9c377
Brendo Gomes <brensogs2021@gmail.com>
2024-08-12 16:43:50 -0300

    configuracao de pix e firebase feito

--- backend/src/Service/PagamentoService.ts
+++ backend/src/Service/PagamentoService.ts
@@ -1,12 +1,20 @@
 import { Payment, MercadoPagoConfig } from 'mercadopago';
+import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-
+import { v4 as uuidv4 } from 'uuid';
 import { TypePagamentoPix } from "../Utils/Types/PropsPagamentoPix";
 dotenv.config();
 
+// Configuração do Mercado Pago
+const mercadoPagoConfig = new MercadoPagoConfig({ accessToken: process.env.TOKEN as string })
+const payment = new Payment(mercadoPagoConfig);
 
-const client = new MercadoPagoConfig({ accessToken: process.env.TOKEN as string });
-const payment = new Payment(client);
+admin.initializeApp({
+    credential: admin.credential.cert('src/Service/secury.json')
+});
+const db = admin.firestore();
+
+
 
 class PagamentoService {
     async execute({
@@ -20,37 +28,47 @@ class PagamentoService {
         nome_lider
     }: TypePagamentoPix) {
 
+        const id = uuidv4();
 
-       try{
-
-        const result = await payment.create({
-            body: {
-                transaction_amount: valor,
-                description: descricao,
-                payment_method_id: 'pix',
-                notification_url: "",
-                payer: {
-                    email: email,
-                    first_name: nome,
-                    phone: {
-                        number: contato
+        try {
+            const result = await payment.create({
+                body: {
+                    transaction_amount: valor,
+                    description: descricao,
+                    payment_method_id: 'pix',
+                    payer: {
+                        email: email,
+                        first_name: nome,
+                        phone: {
+                            number: contato
+                        }
                     }
-                }
-            },
-            requestOptions: { idempotencyKey: Math.floor(Date.now() * Math.random()).toString(36) }
-        })
+                },
+                requestOptions: { idempotencyKey: id }
+            });
+            
+            // Cria ou atualiza o documento no Firestore
+            const docRef = db.collection('compradores').doc(id);
+            await docRef.set({
+                nome,
+                contato,
+                descricao,
+                valor,
+                visao,
+                igreja: igreja === "" || !igreja ? null : igreja,
+                nome_lider: nome_lider === "" || !nome_lider ? null : nome_lider,
+                pago: false
+            });
 
-        return result
+            return result;
 
-       }catch(err){
-        console.log(err);
-        throw new Error('Algo deu errado')
-       }
-
-       
+        } catch (err) {
+            console.error(err);
+            throw new Error('Algo deu errado');
+        }
     }
 }
 
 export {
     PagamentoService
-}
+};
