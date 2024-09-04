409a511d9864370b8cf85bf3913fadebfed188b2
Brendo Gomes <brensogs2021@gmail.com>
2024-09-04 09:17:42 -0300

    colocando descricao do produto

--- frontend/src/app/produto/[id]/page.tsx
+++ frontend/src/app/produto/[id]/page.tsx
@@ -64,6 +64,22 @@ export default function Produto({ params }: { params: { id: string } }) {
                     </div>
                 </div>
             </article>
+            <article className={styled.descricao}>
+                
+                <h2>Descrição</h2>
+                
+                <h3>IDADE PAGANTE</h3>
+                <p>Crianças de até 8 anos de idade estarão isentas do pagamento da pulseira. Porém, em caso de lotação do evento, ficarão os pais ou responsáveis, cientes que tais crianças não terão direito a assentos no evento. Caso assim queiram, o aconselhado é que seja adquirido a pulseira.</p>
+                
+                <h3>CREDENCIAMENTO INFANTIL</h3>
+                <p>
+                    No credenciamento do evento, será obrigatório a apresentação do documento da criança, comprovando a idade do menor. Para fins de conferência, a idade que será considerada,
+                    <strong> é a do dia do evento e não do dia da compra.</strong>
+                </p>
+
+                <h3>REGRAS DE REEMBOLSO</h3>
+                <p>O Art. 49, da Lei 8.078/09, do Código de Defesa do Consumidor, lhe garante um prazo legal de 7 (sete) dias após a compra online, a contar do dia seguinte da aquisição, para solicitar o estorno integral do pagamento efetuado.</p>
+            </article>
         </section>
     )
 }
