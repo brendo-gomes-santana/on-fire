0c224fd095f407ce51f55fde86d47fc16a5b7650
Brendo Gomes Santana <brendogs2021@gmail.com>
2024-09-08 00:27:00 -0300

    alterando capa do produto

--- frontend/src/utils/Produtos/index.ts
+++ frontend/src/utils/Produtos/index.ts
@@ -1,15 +1,14 @@
 import Capa from '../../../public/PERFIL.png'
-import Imagem from '../../../public/PERFIL_02.png'
+import Imagem from '../../../public/segundo_lote.jpg'
 import { InformacoesProps } from '../types/CardProps';
 
 export const Produtos:InformacoesProps[] = [
     {
-        id: 1,
+        id: 2,
         name: 'Pulseira Conferencia 2k24 - 2° lote',
         value: 2525,
         cap: Capa,
         image: Imagem,
-        name_image: 'PERFIL_02.png',
-
+        name_image: 'segundo_lote.jpg',
     }
 ]
